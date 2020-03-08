4b70e7c669386a764478fdf6b97f2d53136faa76
Doug Durham <ddurham@dontpaniclabs.com>
2020-03-08 15:13:08 -0500

    Setup scaffolding for personal project

---
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalProjectLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
