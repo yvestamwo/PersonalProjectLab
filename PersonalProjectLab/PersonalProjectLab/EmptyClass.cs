0f2834b029c7a12b2231a4e3baab683b9c3dab52
yvestamwo <59981914+yvestamwo@users.noreply.github.com>
2020-04-11 17:53:19 -0700

    good

--- PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
+++ PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
@@ -6,5 +6,60 @@ namespace PersonalProjectLab
         public EmptyClass()
         {
         }
-    }
-}
+       
+	
+		
+
+			private double num1;
+			private double num2;
+			private string op;
+
+			// constructor 
+			
+
+			public double addition(double a, double b)
+			{
+				double result = a + b;
+				return result;
+			}
+
+			public double multiplication(double a, double b)
+			{
+				double result = a * b;
+				return result;
+			}
+
+			public double division(double a, double b)
+			{
+				double result = a / b;
+				return result;
+			}
+
+			public void compute(double a, double b, string c)
+			{
+				double result;
+				if (c.Equals("+"))
+				{
+					result = addition(a, b);
+					Console.WriteLine("resul of your operation is " + result);
+				}
+				else if (c.Equals("*"))
+				{
+					result = multiplication(a, b);
+					Console.WriteLine("result of your multiplication is " + result);
+				}
+				else if (c.Equals("/"))
+				{
+					result = division(a, b);
+					Console.WriteLine("result of your operation is " + result);
+				}
+				else
+				{
+					Console.WriteLine("error: wrong input ");
+				}
+			}
+
+		}
+
+	}
+
--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -6,7 +6,47 @@ namespace PersonalProjectLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1 = 0;
+            double number2 = 0;
+            string input3 = "";
+            EmptyClass obj = new EmptyClass();
+            bool boo = false;
+            // Check user inputs 
+            do
+            {
+                Console.WriteLine("enter number1");
+                string input1 = Console.ReadLine();
+                Console.WriteLine("enter number2");
+                string input2 = Console.ReadLine();
+                Console.WriteLine("choose your opperation");
+                input3 = Console.ReadLine();
+
+                try
+                {
+                    number1 = int.Parse(input1);
+                    number2 = int.Parse(input2);
+                    if (input3.Equals("+") | input3.Equals("*") | input3.Equals("/"))
+                    {
+
+                    }
+                    else
+                    {
+                        boo = true;
+                        Console.WriteLine("wrong input");
+                    }
+                }
+                catch (Exception e)
+                {
+                    boo = true;
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("reenter your data");
+                }
+            } while (boo);
+
+            // Computer the operation
+            obj.compute(number1, number2, input3);
+
         }
     }
 }
+        
