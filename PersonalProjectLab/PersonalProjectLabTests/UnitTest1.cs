8a5c18b45cbe91fff22e025511a42c9baead358b
yvestamwo <59981914+yvestamwo@users.noreply.github.com>
2020-04-11 19:14:18 -0700

    Update UnitTest1.cs

--- PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
+++ PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
@@ -8,6 +8,34 @@ namespace PersonalProjectLabTests
         [TestMethod]
         public void TestMethod1()
         {
+            double c = 8;
+            double a = 3;
+            double b = 5;
+            Assert.IsTrue(c == a + b, "TestMethod1 has failed.");
         }
     }
-}
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double c = 15;
+            double a = 3;
+            double b = 5;
+            Assert.IsTrue(c == a * b, "TestMethod2 has failed.");
+        }
+        [TestClass]
+        public class UnitTest3
+        {
+            [TestMethod]
+            public void TestMethod3()
+            {
+                double c = 5;
+                double a = 25;
+                double b = 5;
+                Assert.IsTrue(c == a / b, "TestMethod3 has failed.");
+            }
+        }
+        }
+    }
